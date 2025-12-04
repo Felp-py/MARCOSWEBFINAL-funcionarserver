1ec2f3f78b4c593450867099dffafaed0d282e7d
sol <sol.cl8750@gmail.com>
2025-12-04 18:54:11 -0500

    eliminacion de archivos sin utilizar

--- src/main/java/com/example/demo/repository/RecomendacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Recomendacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecomendacionRepository extends JpaRepository<Recomendacion, Integer> {
-}
--- src/main/java/com/example/demo/service/RecomendacionService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Recomendacion;
-import java.util.List;
-
-public interface RecomendacionService {
-    List<Recomendacion> findAll();
-    Recomendacion save(Recomendacion recomendacion);
-}
